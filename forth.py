c9e7e9e557e2a93b5ae89b984a72a5e2b3597337
Octav Sandulescu <github@narc.ro>
2014-11-01 17:46:04 +0000

    Add experimental new lexer.

--- forth.py
+++ forth.py
@@ -3,21 +3,17 @@ from __future__ import division
 import sys
 import readline
 import operator
+import string
 
 ERROR_FORMAT = ' ? %s %r'
 
 class ForthException(Exception): pass
-
+class BadNumberSyntax(ForthException): pass
 class UnknownWord(ForthException): pass
-
 class UnknownToken(ForthException): pass
-
 class EmptyStack(ForthException): pass
-
 class InsufficientValues(ForthException): pass
-
 class StringMathIsBadAndYouShouldFeelBad(ForthException): pass
-
 class BadParserEndState(ForthException): pass
 
 
@@ -26,6 +22,158 @@ def BadEndState(message):
         decorated.bad_end_state = message
         return decorated
     return BadEndStateDecorator
+
+
+class Lexer(object):
+    """
+    A forth.Lexer will return tokens (which are tuples (type, value)) representing
+    lexically-important parts of the input (presumably Forth) text.
+
+    Example:
+        >>> lex = forth.Lexer('2 3 * .')
+        >>> print lex.tokens()
+    """
+    def __init__(self, text):
+        self.text = text
+        self._tokens = []  # Our current batch of tokens. Often empty.
+
+    def tokens(self):
+        """
+        Generator yielding the tokens in the input text.
+
+        This also drives the lexing process in the first place, by triggering a scan
+        for more lexemes if none are immediately available.
+
+        Lexical errors will be raised as ForthException()s.
+        """
+        self._state = self._lexWhitespace  # The next state method we'll run
+        self._start = 0  # The start of the current token.
+        self._pos = 0  # Our position in the text being lexed.
+
+        while True:
+            if self._tokens:
+                yield self._tokens.pop()
+            else:
+                self._tokenize()
+
+    def _tokenize(self):
+        """
+        Perform the next state in the state machine. Raises StopIteration if there
+        isn't one (for convenience in a generator context).
+        """
+        if self._state is not None:
+            self._state = self._state()
+        else:  # No next state means we're done parsing this text.
+            raise StopIteration()
+
+    def _peek(self):
+        """ Previews the next character of input (but does not consume it). """
+        if self._pos >= len(self.text):
+            return None
+        char = self.text[self._pos]
+        return char
+
+    def _next(self):
+        """ Consumes and returns the next character of input. """
+        char = self._peek()
+        self._pos += 1
+        return char
+
+    def _undo(self):
+        """
+        Undoes a single self._next, effectively un-consuming it.
+        After the end of the string has been consumed, undo is no longer possible.
+        """
+        if self._pos < len(self.text):
+            self._pos -= 1
+
+    def _accept(self, allowed_chars):
+        """ Accept the next character _if_ it's within the given set of allowed_chars. """
+        char = self._next()
+        if char is None:
+            return False # never accept past the end of the string
+        elif char in allowed_chars:
+            return True
+        else:
+            self._undo()
+            return False
+
+    def _accept_run(self, allowed_chars):
+        """ Accept characters until one comes up that's not in the allowed_chars. """
+        while self._accept(allowed_chars):
+            pass  # a little magic, but really nothing to do here.
+
+    @property
+    def _pending_text(self):
+        return self.text[self._start:self._pos]
+
+    def _emit_unless_empty(self, token_type):
+        """ Emit a token only if the current pending text is not empty. """
+        if self._pending_text:
+            self._emit(token_type)
+
+    def _emit(self, token_type):
+        """ Emit a token consisting of the currently pending text (start to pos). """
+        self._tokens.insert(0, (token_type, self._pending_text))
+        self._start = self._pos
+
+    def _ignore(self):
+        """ Dismiss the currently pending text and start a new token. """
+        self._start = self._pos
+
+    def _lexWhitespace(self):
+        char = self._next()
+
+        # Consume character until either a non-whitespace or the end of string
+        while char is not None and char.isspace():
+            char = self._next()
+
+        # Unconsume that last character so we can dispatch correctly.
+        self._undo()  # note: does not undo an end-of-string
+        self._emit_unless_empty('whitespace')
+
+        if char is None:
+            self._emit('eof')
+            return None  # No further states
+        elif char in '+-0123456789':
+            return self._lex_number
+        elif char == '"':
+            return self._lex_quoted_string
+        else:
+            return self._lex_word
+
+    def _lex_number(self):
+        self._accept("+-") # optional leading sign
+        # 123.456 is okay
+        self._accept_run(string.digits)
+        if self._accept("."):
+            self._accept_run(string.digits)
+
+        # 123xyz is not okay
+        char = self._peek()
+        if char is not None and char.isalpha():
+            self._next()
+            raise BadNumberSyntax(self._pending_text, "Unexpected trailing alphabetic character")
+
+        self._emit("number")
+        return self._lexWhitespace
+
+    def _lex_word(self):
+        self._accept_run(string.letters + string.digits)
+        self._emit_unless_empty('word')
+        return self._lexWhitespace
+
+    def _lex_quoted_string(self):
+        self._accept('"')
+        self._ignore()
+        char = self._next()
+        while char is not None and char != '"':
+            char = self._next()
+        self._undo()
+        self._emit_unless_empty('string')
+        self._accept('"')
+        self._ignore()
+        return self._lexWhitespace
 
 
 class ForthParser(object):
